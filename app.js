8959136294be2604bf3f489373bb0c7f1a9bc162
장재성 <jjs@jangjaeseong-ui-MacBookPro.local>
2020-11-03 15:08:45 +0900

    Added Blog Contents

---
+++ app.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var navigation = document.querySelector('.nav-container')
+    var menu = document.querySelector('#toggle')
+    navigation.classList.toggle('active')
+    menu.classList.toggle('active')
+}
